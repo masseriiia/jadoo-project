01c8063d833379868ef8326474af08d69425a4ec
Danil <bloodshoww@mail.ru>
2024-01-11 18:45:38 +0300

    fix: fixed bugs with the adapter

--- src/js/common/burger-menu.js
+++ src/js/common/burger-menu.js
@@ -1,5 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    setBurgerMenu()
+})
+
 const setBurgerMenu = () => {
     const burgerMenu = document.querySelector('.burger__menu')
+    const header = document.querySelector('.header')
     const headerList = document.querySelector('.header__inner')
 
     burgerMenu.addEventListener('click', () => {
@@ -7,9 +13,11 @@ const setBurgerMenu = () => {
         headerList.classList.toggle('open__menu')
         if (document.body.style.overflow === 'hidden') {
             document.body.style.backgroundColor = 'white'
-            document.body.style.overflow = ''
+            header.style.backgroundColor = 'inherit'
+            document.body.style.overflow = 'visible'
         } else {
             document.body.style.overflow = 'hidden'
+            header.style.backgroundColor = 'black'
             document.body.style.backgroundColor = 'black'
         }
     })
@@ -18,9 +26,9 @@ const setBurgerMenu = () => {
         if (window.innerWidth > 1200) {
             burgerMenu.classList.remove('burger__active')
             headerList.classList.remove('open__menu')
+            header.style.backgroundColor = 'inherit'
             document.body.style.backgroundColor = 'white'
             document.body.style.overflow = 'scroll'
         }
     });
 }
-setBurgerMenu()
